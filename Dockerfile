77ad18f40456bfb485a5c1b110f111cf1635706b
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2025-02-21 22:07:40 +0530

    SGN Testing masha Docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,26 @@
 # Set the base image to use for subsequent instructions
-FROM alpine:3.21
+FROM python:3.13-slim-bookworm AS builder
 
 # Set the working directory inside the container
-WORKDIR /usr/src
 
-# Copy any source file(s) required for the action
-COPY entrypoint.sh .
 
-# Configure the container to be run as an executable
-ENTRYPOINT ["/usr/src/entrypoint.sh"]
+WORKDIR /app
+
+RUN pip3 install --target=/app masha==0.0.2
+
+COPY . /app
+
+
+# FROM gcr.io/distroless/python3-debian12
+FROM python:3.13-slim-bookworm
+
+COPY --from=builder /app /app
+
+WORKDIR /app
+
+ENV PYTHONPATH /app
+
+ENTRYPOINT ["/app/entrypoint.sh"]
+# ENTRYPOINT ["/app/masha/cli.py"]
+# CMD ["--help"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,35 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  GITHUB_OUTPUT="/tmp/masha.out"
+fi
+
+function on_err() {
+  echo "::set-output name=exit_code::$1"
+  echo "::set-output name=exit_message::$2"
+  exit $1
+}
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
+cmd="/app/bin/masha"
+if [ -z "$INPUT_VARIABLES" ]; then on_err 1 "INPUT_VARIABLES is not set"; fi 
+variables=$(echo "-v $INPUT_VARIABLES" | sed 's/,/ -v /g')
+cmd="$cmd $variables"
+
+if [ -z "$INPUT_OUTPUT" ]; then on_err 1 "INPUT_OUTPUT is not set"; fi 
+cmd="$cmd $INPUT_OUTPUT"
+
+if [ -z "$INPUT_INPUT_FILE" ]; then on_err 1 "INPUT_INPUT_FILE is not set"; fi 
+cmd="$cmd $INPUT_INPUT_FILE"
 
 # Use workflow commands to do things like set debug messages
-echo "::notice file=entrypoint.sh,line=7::$GREETING"
+echo "Running: $cmd"
 
-# Write outputs to the $GITHUB_OUTPUT file
-echo "greeting=$GREETING" >>"$GITHUB_OUTPUT"
+eval "$cmd" 2>&1 | tee /tmp/test.txt
+retval=${PIPESTATUS[0]}
+if [ $retval -ne 0 ]; then
+  on_err $retval "ERROR Occurred: $(tail -1 /tmp/test.txt)"
+else
+  on_err 0 "Completed Successfully"
+fi
 
-exit 0
